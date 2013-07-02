084e65156d754b614875de0ee593e548757fe804
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-02 18:59:22 -0400

    stubbing out quote

--- stock_spec.rb
+++ stock_spec.rb
@@ -5,6 +5,10 @@ describe 'Stock'  do
 
   end
   describe '#position' do
-
+    it 'returns the position of the stock' do
+      Stock.stub(:quote).and_return(5)
+      stock = Stock.new('AAPL', 20)
+      stock.position.should eq 100
+    end
   end
 end
