50a1aad372c5f5445e078c0446355a9bd039fc6e
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-02 20:04:53 -0400

    Working tests on #position and #quote, although #position needs to be mocked more

--- stock_spec.rb
+++ stock_spec.rb
@@ -2,7 +2,11 @@ require_relative 'stock'
 
 describe 'Stock'  do
   describe '.quote' do
-
+    it 'returns stock quote' do
+      symbol = 'AAPL'
+      YahooFinance.should_receive(:get_quotes).with(YahooFinance::StandardQuote, symbol).and_return(quotes)
+      Stock.quote(symbol).should eq 5
+    end
   end
   describe '#position' do
     it 'returns the position of the stock' do
@@ -10,5 +14,11 @@ describe 'Stock'  do
       stock = Stock.new('AAPL', 20)
       stock.position.should eq 100
     end
+  end
+
+  def quotes
+    {
+      'AAPL' => YahooFinance::StandardQuote.new(['','', 5])
+    }
   end
 end
